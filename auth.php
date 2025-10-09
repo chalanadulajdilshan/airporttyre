b843aad203e5c37b11494e0d3418fcbc6cb42ef0
chalanadulajdilshan <chalanadulaj98@gmail.com>
2025-10-09 15:32:44 +0530

    Add Files

---
+++ auth.php
@@ -0,0 +1,49 @@
+<?php 
+if (!isset($_SESSION)) {
+    // Set session lifetime to 5 hours (in seconds = 18000)
+    ini_set('session.gc_maxlifetime', 18000);   // server-side session data
+    ini_set('session.cookie_lifetime', 18000);  // cookie lifetime in browser
+    
+    session_set_cookie_params(18000); // 5 hours
+    session_start();
+}
+
+
+$USER = new User(NULL);
+if (!$USER->authenticate()) {
+    redirect('login.php');
+}
+
+$USER_PERMISSION = new UserPermission();
+
+// Get the current page
+$current_page = basename($_SERVER['PHP_SELF']);
+
+// Add non-permission pages dynamically
+$NP = new NonPermissionPage();
+$nonPermissionPages = $NP->all(); // fetch all non-permission pages
+
+foreach ($nonPermissionPages as $page) {
+    $skipPages[] = $page['page']; // add page name to skipPages array
+}
+
+// Check access if current page is not in skipPages
+if (!in_array($current_page, $skipPages)) {
+    $page_id = $_GET['page_id'] ?? null;
+    $USER_PERMISSION->checkAccess($page_id);
+}
+
+// Get company details
+$US = new User($_SESSION['id']);
+$company_id = $US->company_id;
+
+$COMPANY_PROFILE_DETAILS = new CompanyProfile($company_id);
+
+// Add account year start date and end date 
+$year_start = '2025-04-01';
+$year_end = '2026-03-31';
+
+$DOCUMENT_TRACKINGS = new DocumentTracking(NULL);
+$doc_id = $DOCUMENT_TRACKINGS->getAllByCompanyAndYear($company_id, $year_start, $year_end);
+
+$PERMISSIONS = $USER_PERMISSION->hasPermission($_SESSION['id'], $page_id ?? 0);
